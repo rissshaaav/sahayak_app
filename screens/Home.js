f7b2ad11f03003f187d616f37b903c1e6471cba2
Rishav <111266613+rissshaaav@users.noreply.github.com>
2023-12-17 18:33:40 +0530

    hide the scrollbar in feed

--- screens/Home.js
+++ screens/Home.js
@@ -24,6 +24,7 @@ const Home = () => {
           renderItem={({item})=>{return(
             <Complaint item={item}/>
           )}}
+          showsVerticalScrollIndicator={false}
         />
       </View>
       <FooterNav />
